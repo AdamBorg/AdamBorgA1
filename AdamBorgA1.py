b13e17c9b0a8e604c85969b7a299a542941c25b9
AdamBorg <adam.borg@my.jcu.edu.au>
2016-09-07 12:30:07 +1000

    add print list function

--- AdamBorgA1.py
+++ AdamBorgA1.py
@@ -10,18 +10,17 @@ def main():
     import csv
 
     open_list = csv.reader(open("items.csv", 'r'))
-    user_input = 'i'
+    user_input = 'Initiate Variable'
 
     item_count = 0
     shopping_list = []
 
     for row in open_list:
         shopping_list.append(row)
-
         item_count += 1
 
     shopping_list = sorted(shopping_list, key = lambda shopping_list: shopping_list[2])
-    print(shopping_list)
+
     print("Hi Welcome to Shopping List 1.0 - by Adam Borg \n{} items loaded from items.csv".format(item_count))
 
     while user_input != 'q':
@@ -30,50 +29,37 @@ def main():
         user_input = str(input().lower())
 
         if user_input == 'c':
-            complete_list(shopping_list)
+            print_lists(shopping_list, user_input)
 
         elif user_input == 'r':
-            registered_list(shopping_list)
+            print_lists(shopping_list, user_input)
 
         elif user_input == 'm':
-            registered_list(shopping_list)
+            print_lists(shopping_list, user_input)
+
             item_selected = int(input("Enter the number of an item to mark as completed?\n"))
             shopping_list[item_selected][3] = 'c'
             print("{} marked as completed".format(shopping_list[item_selected][0]))
             shopping_list = sorted(shopping_list, key=lambda shopping_list: shopping_list[3], reverse=True)
 
-def registered_list(shopping_list):
+
+def print_lists(shopping_list, user_input):
     list_count = -1
     number_of_items = 0
 
+    if user_input == 'm':
+        user_input = 'r'
+
     for item_information in shopping_list:
 
-        if item_information[3] == 'r':
+        if item_information[3] == user_input:
             list_count += 1
             number_of_items += 1
             print("{}. {:17} ${:5.2f} ({})".format(list_count, item_information[0], float(item_information[1]),
                                                    item_information[2]))
     total_cost = 0
     for item_information in shopping_list:
-        if item_information[3] == 'r':
-            total_cost += float(item_information[1])
-
-    print("Total expected price for {} items: $ {:.2f}".format(number_of_items, total_cost))
-
-def complete_list(shopping_list):
-    list_count = -1
-    number_of_items = 0
-
-    for item_information in shopping_list:
-
-        if item_information[3] == 'c':
-            list_count += 1
-            number_of_items += 1
-            print("{}. {:17} ${:5.2f} ({})".format(list_count, item_information[0], float(item_information[1]),
-                                                   item_information[2]))
-    total_cost = 0
-    for item_information in shopping_list:
-        if item_information[3] == 'c':
+        if item_information[3] == user_input:
             total_cost += float(item_information[1])
 
     if total_cost == 0:
