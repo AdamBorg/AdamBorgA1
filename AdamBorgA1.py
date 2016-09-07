4ba8fba6f4ccc56396e405ed50aeeb7a9614ce0a
AdamBorg <adam.borg@my.jcu.edu.au>
2016-09-07 20:49:03 +1000

    add items

--- AdamBorgA1.py
+++ AdamBorgA1.py
@@ -6,52 +6,70 @@
 
 
 def main():
-    import csv
-
     user_input = 'Initiate Variable'
-    open_read = csv.reader(open("items.csv", 'r', ))
 
-    item_count = 0
-    shopping_list = []
+    shopping_list = load_items()
+    shopping_list = sorted(shopping_list, key=lambda shopping_list: shopping_list[2])
 
-    for row in open_read:
-        shopping_list.append(row)
-        item_count += 1
-
-    shopping_list = sorted(shopping_list, key=lambda shopping: shopping_list[2])
-
-    print("Hi Welcome to Shopping List 1.0 - by Adam Borg \n{} items loaded from items.csv".format(item_count))
+    print("Hi Welcome to Shopping List 1.0 - by Adam Borg \n{} items loaded from items.csv".format(len(shopping_list)))
 
     while user_input != 'q':
         print("Menu: \nR - List Required items \nC - List completed items \nA - Add new item \nM - Mark an item as "
               "completed \nQ - Quit")
         user_input = str(input().lower())
 
+        if user_input not in 'rcamq':
+            print("Invalid menu choice")
+
         if user_input == 'c' or user_input == 'r':
             print_lists(shopping_list, user_input)
+            shopping_list = sorted(shopping_list, key=lambda shopping_list: shopping_list[2])
 
         elif user_input == 'm':
-            asd = print_lists(shopping_list, user_input)
+            shopping_list = complete_an_item(shopping_list, user_input)
 
-            if asd != 0:
-                item_selected = int(input("Enter the number of an item to mark as completed?\n"))
-                shopping_list[item_selected][3] = 'c'
-                print("{} marked as completed".format(shopping_list[item_selected][0]))
-                shopping_list = sorted(shopping_list, key=lambda shopping: shopping_list[3], reverse=True)
 
         elif user_input == 'a':
-            add_item = [0, 0, 0, 0]
-            add_item[0] = str(input("Item name: "))
-            add_item[1] = str(input("Price: $"))
-            add_item[2] = str(input("Priority: "))
-            add_item[3] = 'r'
-            shopping_list.append(add_item)
-            shopping_list = sorted(shopping_list, key=lambda shopping: shopping_list[2])
+            shopping_list = add_items(shopping_list)
 
-    open_write = csv.writer(open("items.csv", 'w', newline=''))
+    from csv import writer
+    open_write = writer(open("items.csv", 'w', newline=''))
 
     for items in shopping_list:
         open_write.writerow(items)
+
+
+def add_items(shopping_list):
+    add_item = [0, 0, 0, 0]
+    add_item[0] = str(input("Item name: "))
+    add_item[1] = str(input("Price: $"))
+    add_item[2] = str(input("Priority: "))
+    add_item[3] = 'r'
+    shopping_list.append(add_item)
+    shopping_list = sorted(shopping_list, key=lambda shopping_list: shopping_list[2])
+    print(shopping_list)
+    return shopping_list
+
+
+def complete_an_item(shopping_list, user_input):
+    anymore_items = print_lists(shopping_list, user_input)
+    if anymore_items != 0:
+        shopping_list = sorted(shopping_list, key=lambda shopping_list: shopping_list[3], reverse=True)
+        item_selected = int(input("Enter the number of an item to mark as completed?\n"))
+        shopping_list[item_selected][3] = 'c'
+        print("{} marked as completed".format(shopping_list[item_selected][0]))
+        shopping_list = sorted(shopping_list, key=lambda shopping_list: shopping_list[2])
+    return shopping_list
+
+
+def load_items():
+    from csv import reader
+    open_read = reader(open("items.csv", 'r', ))
+    shopping_list = []
+    for row in open_read:
+        shopping_list.append(row)
+
+    return shopping_list
 
 
 def print_lists(shopping_list, user_input):
